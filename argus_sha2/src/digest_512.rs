f722dd8ba9aa03eb1d9fe6e0674813d06cd769d5
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-06-28 10:39:08 -0700

    initial sha-512 implementation

--- argus_sha2/src/digest_512.rs
+++ argus_sha2/src/digest_512.rs
@@ -1,1 +1,24 @@
 use sha2::{Digest, Sha512};
+
+pub fn build_sha512_object() -> String {
+    let mut hasher = Sha512::new();
+    let message = String::from("hello!");
+
+    hasher.update(&message);
+
+    let built_hash = hasher.finalize();
+
+    hex::encode(&built_hash)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn builder() {
+        let hasher = sha2::Sha512::digest(b"hello!");
+        let hex = hex::encode(&hasher);
+        let sha = build_sha512_object();
+        assert_eq!(hex, sha);
+    }
+}
