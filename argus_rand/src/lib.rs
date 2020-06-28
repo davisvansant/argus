05d6eb9f5046a8ae145d05c3e903eb8e3627ca3a
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-06-28 11:13:13 -0700

    initial rand implementation

--- argus_rand/src/lib.rs
+++ argus_rand/src/lib.rs
@@ -1,7 +1,14 @@
+pub fn generate_salt() -> u64 {
+    rand::random::<u64>()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn generate_and_compare_salts() {
+        let salt_one = generate_salt();
+        let salt_two = generate_salt();
+        assert_ne!(salt_one, salt_two);
     }
 }
