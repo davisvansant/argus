bc5ee5739f2545b4745bcb59ee893feb6e0a6c02
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-06-28 10:36:43 -0700

    initial sha-256 implementation

--- argus_sha2/src/digest_256.rs
+++ argus_sha2/src/digest_256.rs
@@ -1,1 +1,24 @@
 use sha2::{Digest, Sha256};
+
+pub fn build_sha256_object() -> String {
+    let mut hasher = Sha256::new();
+    let message = String::from("hello!");
+
+    hasher.update(&message);
+
+    let built_hash = hasher.finalize();
+
+    hex::encode(&built_hash)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn builder() {
+        let hasher = sha2::Sha256::digest(b"hello!");
+        let hex = hex::encode(&hasher);
+        let sha = build_sha256_object();
+        assert_eq!(hex, sha);
+    }
+}
