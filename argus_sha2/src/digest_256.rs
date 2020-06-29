e81541cce86d7bd388d960e0739a2418d504228d
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-06-29 10:13:24 -0700

    updated sha256 with password and salt parameters, including new tests

--- argus_sha2/src/digest_256.rs
+++ argus_sha2/src/digest_256.rs
@@ -1,10 +1,10 @@
 use sha2::{Digest, Sha256};
 
-pub fn build_sha256_object() -> String {
+pub fn build_sha256_object(password: &String, salt: &String) -> String {
     let mut hasher = Sha256::new();
-    let message = String::from("hello!");
 
-    hasher.update(&message);
+    hasher.update(&password);
+    hasher.update(&salt);
 
     let built_hash = hasher.finalize();
 
@@ -14,11 +14,17 @@ pub fn build_sha256_object() -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use argus_rand::generate_salt;
     #[test]
-    fn builder() {
-        let hasher = sha2::Sha256::digest(b"hello!");
-        let hex = hex::encode(&hasher);
-        let sha = build_sha256_object();
-        assert_eq!(hex, sha);
+    fn generate_sha256_objects_and_compare() {
+        let password_one: String = String::from("password");
+        let password_two: String = String::from("password");
+        let salt_one = generate_salt();
+        let salt_two = generate_salt();
+        let sha_one = build_sha256_object(&password_one, &salt_one);
+        let sha_two = build_sha256_object(&password_two, &salt_two);
+        let sha_three = build_sha256_object(&password_two, &salt_one);
+        assert_ne!(sha_one, sha_two);
+        assert_eq!(sha_one, sha_three);
     }
 }
