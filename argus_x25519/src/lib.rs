17264da10dfaa2a6aa751a208dc0b1e805f667be
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-06-28 17:54:47 -0700

    initial x25519 implementation

--- argus_x25519/src/lib.rs
+++ argus_x25519/src/lib.rs
@@ -1,7 +1,26 @@
+use rand_os::OsRng;
+use x25519_dalek::{EphemeralSecret, PublicKey};
+
+pub fn generate_ephermeral_secret() -> EphemeralSecret {
+    let mut rng = OsRng::new().unwrap();
+    x25519_dalek::EphemeralSecret::new(&mut rng)
+}
+
+pub fn generate_public_key(secret: &EphemeralSecret) -> PublicKey {
+    x25519_dalek::PublicKey::from(secret)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn compare_dh_keys() {
+        let alice_secret = generate_ephermeral_secret();
+        let bob_secret = generate_ephermeral_secret();
+        let alice_public = generate_public_key(&alice_secret);
+        let bob_public = generate_public_key(&bob_secret);
+        let alice_shared_secret = alice_secret.diffie_hellman(&bob_public);
+        let bob_shared_secret = bob_secret.diffie_hellman(&alice_public);
+        assert_eq!(alice_shared_secret.as_bytes(), bob_shared_secret.as_bytes());
     }
 }
