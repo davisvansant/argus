cf07e07157c180b78b7cb1b7fa14e54e05597564
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-06-29 10:16:56 -0700

    adjust sha256 naming

--- argus_sha2/src/digest_256.rs
+++ argus_sha2/src/digest_256.rs
@@ -1,6 +1,6 @@
 use sha2::{Digest, Sha256};
 
-pub fn build_sha256_object(password: &String, salt: &String) -> String {
+pub fn build_object(password: &String, salt: &String) -> String {
     let mut hasher = Sha256::new();
 
     hasher.update(&password);
@@ -16,14 +16,14 @@ mod tests {
     use super::*;
     use argus_rand::generate_salt;
     #[test]
-    fn generate_sha256_objects_and_compare() {
+    fn generate_objects_and_compare() {
         let password_one: String = String::from("password");
         let password_two: String = String::from("password");
         let salt_one = generate_salt();
         let salt_two = generate_salt();
-        let sha_one = build_sha256_object(&password_one, &salt_one);
-        let sha_two = build_sha256_object(&password_two, &salt_two);
-        let sha_three = build_sha256_object(&password_two, &salt_one);
+        let sha_one = build_object(&password_one, &salt_one);
+        let sha_two = build_object(&password_two, &salt_two);
+        let sha_three = build_object(&password_two, &salt_one);
         assert_ne!(sha_one, sha_two);
         assert_eq!(sha_one, sha_three);
     }
