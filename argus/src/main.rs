f49810f34f574f6f91af343c828df38c0724fe67
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-06-30 15:16:02 -0700

    add some initial work around entry

--- argus/src/main.rs
+++ argus/src/main.rs
@@ -1,3 +1,33 @@
+use std::collections::HashMap;
+use std::io;
+use std::io::Write;
+
+struct User {
+    name: String,
+    id: String,
+}
+
+impl User {
+    fn new(name: &str, id: &str) -> User {
+        User {
+            name: name.to_string(),
+            id: id.to_string(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Welcome to Argus");
+
+        let mut x = String::new();
+        print!("----> ",);
+        io::stdout().flush();
+        io::stdin().read_line(&mut x).expect("Failed to read line");
+
+        match x.trim().parse::<i32>().unwrap() {
+            1 => println!("create new user"),
+            _ => return,
+        };
+    }
 }
