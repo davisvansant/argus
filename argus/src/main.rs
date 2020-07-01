7a06b3eb735894527f0063c623f27cb7044d99d8
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-07-01 15:58:02 -0700

    some initial work around user/password creation

--- argus/src/main.rs
+++ argus/src/main.rs
@@ -20,6 +20,51 @@ fn main() {
     loop {
         println!("Welcome to Argus");
 
+        let mut new_user_name = String::new();
+
+        println!("[ argus ] Please enter your name ----> ",);
+        io::stdout().flush();
+        io::stdin()
+            .read_line(&mut new_user_name)
+            .expect("Failed to read line");
+
+        loop {
+            println!("[ argus ] Please enter your password ----> ");
+
+            let mut new_user_password = String::new();
+
+            io::stdout().flush();
+            io::stdin()
+                .read_line(&mut new_user_password)
+                .expect("Failed to read line");
+
+            let mut new_user_password_again = String::new();
+            io::stdout().flush();
+
+            println!("[ argus ] Please enter your password ----> ");
+            io::stdout().flush();
+            io::stdin()
+                .read_line(&mut new_user_password_again)
+                .expect("Failed to read line");
+
+            if new_user_password == new_user_password_again {
+                println!("good!");
+                break;
+            } else {
+                println!("not good!");
+                continue;
+            }
+        }
+
+        println!("[ argus ] creating key...");
+
+        println!("[ argus ] creating another key....");
+
+        println!("----> 1 - Create new item");
+        println!("----> 2 - View Current items");
+        println!("----> 3 - Add another user for testing");
+        println!("----> 4 - Quit");
+
         let mut x = String::new();
         print!("----> ",);
         io::stdout().flush();
@@ -27,7 +72,14 @@ fn main() {
 
         match x.trim().parse::<i32>().unwrap() {
             1 => println!("create new user"),
-            _ => return,
+            2 => println!("view current items!"),
+            3 => println!("add another user!"),
+            4 => {
+                println!("----> Goodbye ...");
+                print!("{}[2J", 27 as char);
+                return;
+            }
+            _ => continue,
         };
     }
 }
