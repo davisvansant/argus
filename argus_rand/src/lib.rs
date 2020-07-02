1a5f8fbb4f55d634d8fac5985a5a21d8399c3a90
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-07-02 14:36:57 -0700

    initial 'generate_pin()' for argus_rand

--- argus_rand/src/lib.rs
+++ argus_rand/src/lib.rs
@@ -25,6 +25,12 @@ pub fn generate_osrng() -> OsRng {
     OsRng {}
 }
 
+pub fn generate_pin() -> String {
+    let mut random = rand::thread_rng();
+    let pin: String = random.gen_range(000000, 999999).to_string();
+    pin
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -33,5 +39,14 @@ mod tests {
         let salt_one = generate_salt();
         let salt_two = generate_salt();
         assert_ne!(salt_one, salt_two);
+    }
+
+    #[test]
+    fn generate_and_compare_pins() {
+        let pin_one = generate_pin();
+        let pin_two = generate_pin();
+        assert!(pin_one.len() == 6);
+        assert!(pin_two.len() == 6);
+        assert_ne!(pin_one, pin_two);
     }
 }
