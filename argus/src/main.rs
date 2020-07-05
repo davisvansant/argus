6cc079882098ece5f7b8fc7babc898c43505bb62
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-07-05 13:20:45 -0700

    initial cleanup of argus main

--- argus/src/main.rs
+++ argus/src/main.rs
@@ -1,37 +1,37 @@
-use std::collections::HashMap;
+// use std::collections::HashMap;
 use std::io;
 use std::io::Write;
 
-struct User {
-    name: String,
-    account_number: String,
-    pin: String,
-}
-
-impl User {
-    fn new(name: &str, account_number: &str, pin: &str) -> User {
-        User {
-            // this can take user input
-            name: name.to_string(),
-            // argus_rand::generate_account_number();
-            account_number: account_number.to_string(),
-            // argus_rand::generate_pin();
-            pin: pin.to_string(),
-        }
-    }
-
-    fn link(
-        account_number: &str,
-        hash: &str,
-        account_pin: &str,
-        salt: &str,
-    ) -> HashMap<String, String> {
-        let mut link: HashMap<String, String> = HashMap::new();
-        link.insert((&account_number).to_string(), (&hash).to_string());
-        link.insert((&account_pin).to_string(), (&salt).to_string());
-        link
-    }
-}
+// struct User {
+//     name: String,
+//     account_number: String,
+//     pin: String,
+// }
+//
+// impl User {
+//     fn new(name: &str, account_number: &str, pin: &str) -> User {
+//         User {
+//             // this can take user input
+//             name: name.to_string(),
+//             // argus_rand::generate_account_number();
+//             account_number: account_number.to_string(),
+//             // argus_rand::generate_pin();
+//             pin: pin.to_string(),
+//         }
+//     }
+//
+//     fn link(
+//         account_number: &str,
+//         hash: &str,
+//         account_pin: &str,
+//         salt: &str,
+//     ) -> HashMap<String, String> {
+//         let mut link: HashMap<String, String> = HashMap::new();
+//         link.insert((&account_number).to_string(), (&hash).to_string());
+//         link.insert((&account_pin).to_string(), (&salt).to_string());
+//         link
+//     }
+// }
 
 fn main() {
     loop {
@@ -73,41 +73,41 @@ fn main() {
         //     }
         // }
 
-        let test_account_number = String::from("account - 12345");
-        let test_account_pin = String::from("pin - 12345");
-        let test_salt = String::from("salt - 12345");
-        let test_hash = String::from("hash - 12345");
+        // let test_account_number = String::from("account - 12345");
+        // let test_account_pin = String::from("pin - 12345");
+        // let test_salt = String::from("salt - 12345");
+        // let test_hash = String::from("hash - 12345");
+        //
+        // let test_user: User = User::new(
+        //     &new_user_name,
+        //     &(*test_account_number).to_string(),
+        //     &(*test_account_pin).to_string(),
+        // );
+        //
+        // let some_awesome_hash = User::link(
+        //     &test_account_number,
+        //     &test_hash,
+        //     &test_account_pin,
+        //     &test_salt,
+        // );
 
-        let test_user: User = User::new(
-            &new_user_name,
-            &(*test_account_number).to_string(),
-            &(*test_account_pin).to_string(),
-        );
-
-        let some_awesome_hash = User::link(
-            &test_account_number,
-            &test_hash,
-            &test_account_pin,
-            &test_salt,
-        );
-
-        println!("[ argus ] Hello {:?}", test_user.name);
-
-        println!(
-            "[ argus ] Your account number is {:?}",
-            test_user.account_number
-        );
-        println!("{:?}", test_user.account_number);
+        // println!("[ argus ] Hello {:?}", test_user.name);
+        //
+        // println!(
+        //     "[ argus ] Your account number is {:?}",
+        //     test_user.account_number
+        // );
+        // println!("{:?}", test_user.account_number);
 
         println!("[ argus ] creating PIN...");
-        println!("{:?}", test_user.pin);
+        // println!("{:?}", test_user.pin);
 
         // create pin here from argus_rand::generate_pin();
         // store pin, salt, hash for reuse
 
-        for (k, v) in &some_awesome_hash {
-            println!("{} {}", k, v);
-        }
+        // for (k, v) in &some_awesome_hash {
+        //     println!("{} {}", k, v);
+        // }
 
         println!("[ argus ] creating Key....");
 
