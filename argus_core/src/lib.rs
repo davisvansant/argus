72f3b5048858114fa8fec7098da1d5014516c899
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-07-09 14:24:43 -0700

    update core user struct with static secret, shared secret, and sign

--- argus_core/src/lib.rs
+++ argus_core/src/lib.rs
@@ -3,7 +3,7 @@ pub mod system;
 pub struct User {
     pub account_number: String,
     pin: String,
-    x25519_secret: argus_x25519::EphemeralSecret,
+    x25519_secret: argus_x25519::StaticSecret,
     salt: String,
     ed25519_secret_key: argus_ed25519::ExpandedSecretKey,
 }
@@ -19,7 +19,8 @@ impl User {
         Self {
             account_number: argus_rand::generate_account_number(),
             pin: argus_rand::generate_pin(),
-            x25519_secret: argus_x25519::generate_ephermeral_secret(),
+            // x25519_secret: argus_x25519::generate_ephermeral_secret(),
+            x25519_secret: argus_x25519::generate_static_secret(),
             salt: argus_rand::generate_salt(),
             ed25519_secret_key: argus_ed25519::generate_expanded_secret_key(),
         }
@@ -30,11 +31,26 @@ impl User {
     }
 
     pub fn public_key(&self) -> argus_x25519::PublicKey {
-        argus_x25519::generate_public_key(&self.x25519_secret)
+        argus_x25519::generate_public_key_from_static_secret(&self.x25519_secret)
+    }
+
+    pub fn x25519_shared_secret(
+        &self,
+        public_key: &argus_x25519::PublicKey,
+    ) -> argus_x25519::SharedSecret {
+        self.x25519_secret.diffie_hellman(public_key)
     }
 
     pub fn ed25519_public_key(&self) -> argus_ed25519::PublicKey {
         argus_ed25519::generate_public_key_from_secret_key(&self.ed25519_secret_key)
+    }
+
+    pub fn sign(
+        &self,
+        message: &[u8],
+        public_key: argus_ed25519::PublicKey,
+    ) -> argus_ed25519::Signature {
+        argus_ed25519::ExpandedSecretKey::sign(&self.ed25519_secret_key, &message, &public_key)
     }
 }
 
@@ -54,20 +70,13 @@ mod tests {
         let test_system_public = test_system.public_key();
         let test_user_ed25519_public_key = test_user.ed25519_public_key();
         let test_system_ed25519_public_key = test_system.ed25519_public_key();
-        let test_user_shared_secret = test_user.x25519_secret.diffie_hellman(&test_system_public);
-        let test_system_shared_secret = test_system.x25519_secret.diffie_hellman(&test_user_public);
+        let test_user_shared_secret = test_user.x25519_shared_secret(&test_system_public);
+        let test_system_shared_secret = test_system.x25519_shared_secret(&test_user_public);
         let user_message: &[u8] = b"This is a test from user";
         let system_message: &[u8] = b"This is a test from system";
-        let test_user_signature = argus_ed25519::ExpandedSecretKey::sign(
-            &test_user.ed25519_secret_key,
-            &user_message,
-            &test_user_ed25519_public_key,
-        );
-        let test_system_signature = argus_ed25519::ExpandedSecretKey::sign(
-            &test_system.ed25519_secret_key,
-            &system_message,
-            &test_system_ed25519_public_key,
-        );
+        let test_user_signature = test_user.sign(&user_message, test_user_ed25519_public_key);
+        let test_system_signature =
+            test_system.sign(&system_message, test_system_ed25519_public_key);
         assert!(argus_ed25519::PublicKey::verify(
             &test_user_ed25519_public_key,
             &user_message,
@@ -82,8 +91,8 @@ mod tests {
         .is_ok());
         assert_ne!(test_user_sha, test_system_sha);
         assert_eq!(
-            test_user_shared_secret.as_bytes(),
-            test_system_shared_secret.as_bytes()
+            &test_user_shared_secret.as_bytes(),
+            &test_system_shared_secret.as_bytes()
         );
         assert_eq!(users.len(), 2)
     }
