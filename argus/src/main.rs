6d8f4527e43e97df86fc28062775dc11a7e3f15f
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-07-11 13:29:36 -0700

    some cleanup and more structure to UX

--- argus/src/main.rs
+++ argus/src/main.rs
@@ -6,12 +6,11 @@ fn main() {
     let system_x25519_public_key = system.x25519_public_key();
 
     loop {
+        print!("{}[2J", 27 as char);
         println!("Welcome to Argus");
         println!("----> 1 - Create New User");
-        println!("----> 2 - View Current Secrets");
-        println!("----> 3 - Add another user for testing");
-        println!("----> 4 - Generate New PIN");
-        println!("----> 5 - Quit");
+        println!("----> 2 - Login with ID and PIN");
+        println!("----> 3 - Quit");
 
         let mut x = String::new();
         print!("----> ",);
@@ -28,17 +27,41 @@ fn main() {
                 io::stdin()
                     .read_line(&mut new_user_name)
                     .expect("Failed to read line");
-
                 println!("[ argus ] creating PIN...");
-
                 println!("[ argus ] creating Key....");
+            }
+            2 => {
+                print!("{}[2J", 27 as char);
+                println!("login with id and pin");
+                loop {
+                    println!("Welcome username");
+                    println!("[ argus ] Secure connection verified ....");
+                    println!("----> 1 - Create New Secret");
+                    println!("----> 2 - View current secrets");
+                    println!("----> 3 - Logout");
 
-                // println!("[ argus ] Secure connection verified ....");
-            },
-            2 => println!("view current items!"),
-            3 => println!("add another user!"),
-            4 => println!("generate new pin"),
-            5 => {
+                    let mut x = String::new();
+                    print!("----> ",);
+                    io::stdout().flush().unwrap();
+                    io::stdin().read_line(&mut x).expect("Failed to read line");
+
+                    match x.trim().parse::<i32>().unwrap() {
+                        1 => {
+                            println!("create new secrets",);
+                        }
+                        2 => {
+                            println!("view current secrets",);
+                        }
+                        3 => {
+                            println!("----> Goodbye ...");
+                            print!("{}[2J", 27 as char);
+                            break;
+                        }
+                        _ => continue,
+                    }
+                }
+            }
+            3 => {
                 println!("----> Goodbye ...");
                 print!("{}[2J", 27 as char);
                 return;
