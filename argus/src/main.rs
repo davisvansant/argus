5aad7945bb633cb8865263e7a1cf52f6004ce803
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-07-12 19:16:26 -0700

    some initial work around creating a user, but goes nowhere

--- argus/src/main.rs
+++ argus/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::io::Write;
+use std::{thread, time};
 
 fn main() {
     let system = argus_core::system::System::init();
@@ -27,15 +28,29 @@ fn main() {
                 io::stdin()
                     .read_line(&mut new_user_name)
                     .expect("Failed to read line");
+
+                let new_user = argus_core::user::User::new();
                 println!("[ argus ] creating PIN...");
-                println!("[ argus ] creating Key....");
+                let new_user_sha = new_user.sha();
+                println!("[ argus ] creating Keys....");
+                let new_user_public_key = new_user.public_key();
+                let new_user_ed25519_public_key = new_user.ed25519_public_key();
+                println!("[ argus ] Secure connection verified ....");
+                let test_user_shared_secret =
+                    new_user.x25519_shared_secret(&system_x25519_public_key);
+                println!("{:?}", new_user.account_number);
+                // println!("{:?}", new_user.pin);
+                println!("{:?}", new_user_sha);
+                println!("{:?}", new_user_public_key.as_bytes());
+                println!("{:?}", new_user_ed25519_public_key.as_bytes());
+                thread::sleep_ms(10000);
             }
             2 => {
                 print!("{}[2J", 27 as char);
                 println!("login with id and pin");
                 loop {
                     println!("Welcome username");
-                    println!("[ argus ] Secure connection verified ....");
+
                     println!("----> 1 - Create New Secret");
                     println!("----> 2 - View current secrets");
                     println!("----> 3 - Logout");
