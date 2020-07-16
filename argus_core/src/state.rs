fd9e9eb5ac039346b73a8c42315272c69b0e9bd4
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-07-15 21:00:05 -0700

    initial struct and impl for state, initial tests

--- argus_core/src/state.rs
+++ argus_core/src/state.rs
@@ -1,1 +1,26 @@
-// tbd
+use std::collections::HashMap;
+
+struct State {
+    account_salt_and_sha: HashMap<String, HashMap<String, String>>,
+    account_secrets: HashMap<String, HashMap<String, &'static [u8]>>,
+}
+
+impl State {
+    pub fn init() -> Self {
+        Self {
+            account_salt_and_sha: HashMap::new(),
+            account_secrets: HashMap::new(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn create_and_verify_state() {
+        let test_state: State = State::init();
+        assert_eq!(test_state.account_salt_and_sha.len(), 0);
+        assert_eq!(test_state.account_secrets.len(), 0);
+    }
+}
