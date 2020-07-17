901a033166f52e18704f76735820cd21a737aa7e
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-07-16 21:48:23 -0700

    initial implementation of load_account_information method

--- argus_core/src/state.rs
+++ argus_core/src/state.rs
@@ -22,6 +22,20 @@ impl State {
         self.account_salt_and_sha
             .insert(account.id.to_string(), account_information);
     }
+
+    pub fn load_account_information(&mut self, account: &str) -> &mut HashMap<String, String> {
+        if self.account_salt_and_sha.contains_key(&account.to_string()) {
+            println!("loading account - {:?}", account);
+            self.account_salt_and_sha
+                .get_mut(&account.to_string())
+                .unwrap()
+        } else {
+            println!("account not found!");
+            self.account_salt_and_sha
+                .get_mut(&account.to_string())
+                .unwrap() // need to handle - this panics
+        }
+    }
 }
 
 #[cfg(test)]
