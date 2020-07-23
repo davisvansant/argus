d3b55e246b1d40baf10a1851657899f9e99f16e8
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-07-23 10:30:17 -0700

    initial impl of argus_uuid

--- argus_uuid/src/lib.rs
+++ argus_uuid/src/lib.rs
@@ -1,7 +1,18 @@
+pub use uuid::Uuid;
+
+pub fn generate() -> Uuid {
+    Uuid::new_v4()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn generate_uuid_and_compare() {
+        let uuid_one = crate::generate();
+        let uuid_two = crate::generate();
+        let nil = Uuid::nil();
+        assert_ne!(uuid_one, nil);
+        assert_ne!(uuid_one, uuid_two);
     }
 }
