bcc2b468d505557b7dc424f2e69ca1b48e4430c6
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-07-29 14:29:14 -0700

    initial addition of aes_key and nonce functions, with tests

--- argus_rand/src/lib.rs
+++ argus_rand/src/lib.rs
@@ -35,6 +35,42 @@ pub fn generate_account_number() -> String {
     account_number
 }
 
+pub fn generate_aes_key() -> String {
+    let pool: &[u8] = b"\
+    ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+    abcdefghijklmnopqrstuvwxyz\
+    0123456789\
+    ";
+
+    let aes_key_length: usize = 32;
+    let mut rng = rand::thread_rng();
+    let aes_key: String = (0..aes_key_length)
+        .map(|_| {
+            let index = rng.gen_range(0, pool.len());
+            pool[index] as char
+        })
+        .collect();
+    aes_key
+}
+
+pub fn generate_nonce() -> String {
+    let pool: &[u8] = b"\
+    ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+    abcdefghijklmnopqrstuvwxyz\
+    0123456789\
+    ";
+
+    let nonce_length: usize = 12;
+    let mut rng = rand::thread_rng();
+    let nonce: String = (0..nonce_length)
+        .map(|_| {
+            let index = rng.gen_range(0, pool.len());
+            pool[index] as char
+        })
+        .collect();
+    nonce
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -57,5 +93,23 @@ mod tests {
         let account_one = generate_account_number();
         let account_two = generate_account_number();
         assert_ne!(account_one, account_two);
+    }
+
+    #[test]
+    fn generate_and_compare_aes_keys() {
+        let aes_key_one = generate_aes_key();
+        let aes_key_two = generate_aes_key();
+        assert_eq!(aes_key_one.len(), 32);
+        assert_eq!(aes_key_two.len(), 32);
+        assert_ne!(aes_key_one, aes_key_two);
+    }
+
+    #[test]
+    fn generate_and_compare_nonces() {
+        let nonce_one = generate_nonce();
+        let nonce_two = generate_nonce();
+        assert_eq!(nonce_one.len(), 12);
+        assert_eq!(nonce_two.len(), 12);
+        assert_ne!(nonce_one, nonce_two);
     }
 }
