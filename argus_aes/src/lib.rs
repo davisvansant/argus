360d881c1dc3c201e26cac535c86b66c2897c751
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-07-31 13:07:28 -0700

    comment out currently unused tests

--- argus_aes/src/lib.rs
+++ argus_aes/src/lib.rs
@@ -1,9 +1,9 @@
 pub mod gcm;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
